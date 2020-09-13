b4f02fef042ddd2e9bbb4b5ea01eb03ebc01c3b9
JasmeetKaur <jasmeetchadha1990@gmail.com>
2020-09-13 12:47:57 +0530

    This is to handle Authentication Popups

---
+++ AuthPopUp.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AuthPopUp {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+			
+		String username = "admin";
+		String password = "admin";
+		driver.get("http://"+username+":"+password+"@"+"the-internet.herokuapp.com/basic_auth");
+
+	}
+
+}
